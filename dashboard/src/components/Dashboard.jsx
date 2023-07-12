82493b09eb9c8041ba05d0831073bbf34c053622
swash0 <bhandariswastika79@gmail.com>
2023-07-12 12:44:17 +0545

    added widgets on dashboard

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import Users from './Users';
-import Packages from './Packages';
-import TotalSales from './TotalSales';
-import Bookings from './Bookings';
-
-
-
 import { Link } from 'react-router-dom';
-import CustomLink from './CustomLink';
+import Widget from './Widget';
+import CustomLink from './utils/CustomLink';
 
 const Dashboard = () => {
   return (
@@ -15,36 +9,28 @@ const Dashboard = () => {
       <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="bg-white p-4 shadow-md rounded-md">
-          {/* <CustomLink to="/users"> */}
-          <button className="w-full bg-blue-500 hover:bg-blue-600 text-black font-semibold py-2 px-4 rounded-md">
-            Users
-          </button>
-          {/* </CustomLink> */}
+        <div className="bg-white p-2 shadow-md rounded-md">
+          <CustomLink to="/users">
+            <Widget type="user" />
+          </CustomLink>
         </div>
 
-        <div className="bg-white p-4 shadow-md rounded-md">
-          {/* <Link to="/packages"> */}
-          <button className="w-full bg-green-500 hover:bg-green-600 text-black font-semibold py-2 px-4 rounded-md">
-            Packages
-          </button>
-          {/* </Link> */}
+        <div className="bg-white p-2 shadow-md rounded-md">
+          <Link to="/packages">
+            <Widget type="order" />
+          </Link>
         </div>
 
-        <div className="bg-white p-4 shadow-md rounded-md">
-          {/* <Link to="/totalsales"> */}
-          <button className="w-full bg-purple-500 hover:bg-purple-600 text-black font-semibold py-2 px-4 rounded-md">
-            Total Sales
-          </button>
-          {/* </Link> */}
+        <div className="bg-white p-2 shadow-md rounded-md">
+          <Link to="/totalsales">
+            <Widget type="earning" />
+          </Link>
         </div>
 
-        <div className="bg-white p-4 shadow-md rounded-md">
-          {/* <Link to="/bookings"> */}
-          <button className="w-full bg-red-500 hover:bg-red-600 text-black font-semibold py-2 px-4 rounded-md">
-            Bookings
-          </button>
-          {/* </Link> */}
+        <div className="bg-white p-2 shadow-md rounded-md">
+          <Link to="/bookings">
+            <Widget type="balance" />
+          </Link>
         </div>
       </div>
     </div>
---
+++ dashboard/src/components/Widget.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+
+const Widget = ({ type }) => {
+  const dataMap = {
+    user: {
+      title: 'USERS',
+      isMoney: false,
+      link: 'See all users',
+      icon: (
+        <PersonOutlinedIcon
+          className="icon"
+          style={{
+            color: 'crimson',
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+          }}
+        />
+      ),
+    },
+    order: {
+      title: 'PACKAGES',
+      isMoney: false,
+      link: 'View all packages',
+      icon: (
+        <ShoppingCartOutlinedIcon
+          className="icon"
+          style={{
+            backgroundColor: 'rgba(218, 165, 32, 0.2)',
+            color: 'goldenrod',
+          }}
+        />
+      ),
+    },
+    earning: {
+      title: 'EARNINGS',
+      isMoney: true,
+      link: 'View net earnings',
+      icon: (
+        <MonetizationOnOutlinedIcon
+          className="icon"
+          style={{ backgroundColor: 'rgba(0, 128, 0, 0.2)', color: 'green' }}
+        />
+      ),
+    },
+    balance: {
+      title: 'BOOKINGS',
+      isMoney: true,
+      link: 'See details',
+      icon: (
+        <AccountBalanceWalletOutlinedIcon
+          className="icon"
+          style={{
+            backgroundColor: 'rgba(128, 0, 128, 0.2)',
+            color: 'purple',
+          }}
+        />
+      ),
+    },
+  };
+
+  const widgetData = dataMap[type] || {};
+
+  const { title, isMoney, link, icon } = widgetData;
+  const amount = 15; // Temporary amount rakhya
+  const diff = 20; // Temporary difference too 
+
+  return (
+    <div className="widget p-2 shadow-md rounded-md flex justify-between">
+      <div className="left">
+        <span className="title font-bold text-gray-500">{title}</span>
+        <span className="counter text-2xl font-bold">
+          {isMoney && '$'} {amount}
+        </span>
+        <span className="link text-sm border-b border-gray-300">{link}</span>
+      </div>
+      <div className="right flex items-center">
+        <div
+          className={`percentage ${diff > 0 ? 'positive text-green-500' : 'negative text-red-500'
+            }`}
+        >
+          <KeyboardArrowUpIcon className="mr-1" />
+          {diff} %
+        </div>
+        {icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
