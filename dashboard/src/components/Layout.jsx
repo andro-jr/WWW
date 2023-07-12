da38caee2deebadab2c3fc2d4f8eb88fbafefa42
prabin <prabin20panta@gmail.com>
2023-07-12 12:56:01 +0545

    dashboard folder changes

--- dashboard/src/components/Layout.jsx
+++ dashboard/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import Sidebar from './sidebar/Sidebar';
 import '../index.css';
 import { Route, Routes } from 'react-router';
-import Dashboard from './Dashboard';
+import Dashboard from './dashboard/Dashboard';
 import Users from './Users/Users';
 import Packages from './Packages';
 import Navbar from './Navbar/Navbar';
